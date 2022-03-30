58d2b37f1fac1c6ed1b72517d5bf5c810f303429
Dalmo Veras <verasnt@protonmail.com>
2022-03-30 17:16:06 -0300

    add open fn

--- loader.cc
+++ loader.cc
@@ -15,3 +15,19 @@ void unload_binary(Binary *bin){
     }
   }
 }
+
+static bfd* open_bfd(std::string &fname){
+  static int bfd_inited = 0;
+  bfd *bfd_h;
+  
+  if(!bfd_inited){
+    bfd_init();
+    bfd_inited = 1;
+  }
+
+  bfd_h = bfd_openr(fname.c_str(), NULL);
+  if(!bfd_h){
+    fprintf(stderr, "Failed to open binary '%s' (%s)\n", fname.c_str(), bfd_errmsg(bfd_get_error()));
+    return NULL;
+  }
+}
