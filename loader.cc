016a5a733d4875e00d879ea49a302922b918d481
Dalmo Veras <verasnt@protonmail.com>
2022-03-30 17:08:38 -0300

    add some implementation to the BL

---
+++ loader.cc
@@ -0,0 +1,17 @@
+#include <bfd.h>
+#include "loader.h"
+
+int load_binary(std::string &fname, Binary *bin, Binary::BinaryType type){
+  return load_binary_bfd(fname, bin, type);
+}
+
+void unload_binary(Binary *bin){
+  size_t i;
+  Section *sec;
+  for(i=0; i< bin->sections.size(); i++){
+    sec = &bin->sections[i];
+    if(sec->bytes){
+      free(sec->bytes);
+    }
+  }
+}
